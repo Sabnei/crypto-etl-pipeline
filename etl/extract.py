9567a73347bca2dd35cc1cee7e49eb2146ac0333
Sabnei <sebasplay417@gmail.com>
2026-04-23 15:00:01 -0600

    Add data extraction function to fetch cryptocurrency data from CoinGecko API

---
+++ etl/extract.py
@@ -0,0 +1,19 @@
+import requests
+import pandas as pd
+
+url = "https://api.coingecko.com/api/v3/coins/markets"
+PARAMS = {
+    "vs_currency": "usd",
+    "order": "market_cap_desc",
+    "per_page": 10,
+    "page": 1,
+}
+
+def fetch_crypto_data():
+    try:
+        response = requests.get(url, params=PARAMS, timeout=10)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return []
