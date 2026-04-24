3e36130db117a408684ff6f8b88c662bdfc29bfa
Sabnei <sebasplay417@gmail.com>
2026-04-24 03:16:44 -0600

    Increase column sizes for coin_id and name in crypto_prices table

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,9 +1,9 @@
 -- Create a new table 'crypto_prices' with a primary key and columns
 CREATE TABLE crypto_prices (
     id SERIAL PRIMARY KEY,
-    coin_id VARCHAR(50) NOT NULL,
+    coin_id VARCHAR(150) NOT NULL,
     symbol varchar(50) NOT NULL,
-    name varchar(100) NOT NULL,
+    name varchar(150) NOT NULL,
     price_usd DECIMAL(20, 8) NOT NULL,
     market_cap_usd BIGINT,
     volume_24h BIGINT,
