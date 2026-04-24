b68821ca7bc550b6271d7d2a8ed8f6669779ded1
Sabnei <sebasplay417@gmail.com>
2026-04-24 02:30:02 -0600

    Fix SQL syntax by adding a missing comma in the crypto_prices table definition

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE crypto_prices (
     symbol varchar(50) NOT NULL,
     name varchar(100) NOT NULL,
     price_usd DECIMAL(20, 8) NOT NULL,
-    market_cap_usd BIGINT
+    market_cap_usd BIGINT,
     volume_24h BIGINT,
     change_pct_24h DECIMAL(8, 4),
     last_updated_api TIMESTAMPTZ,
