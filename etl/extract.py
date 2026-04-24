3afb3385fdb48f57716fefd3ad9ef059caf2fd06
Sabnei <sebasplay417@gmail.com>
2026-04-24 02:46:54 -0600

    Update API parameters to fetch 100 cryptocurrency entries instead of 10

--- etl/extract.py
+++ etl/extract.py
@@ -5,7 +5,7 @@ url = "https://api.coingecko.com/api/v3/coins/markets"
 PARAMS = {
     "vs_currency": "usd",
     "order": "market_cap_desc",
-    "per_page": 10,
+    "per_page": 100,
     "page": 1,
 }
 
